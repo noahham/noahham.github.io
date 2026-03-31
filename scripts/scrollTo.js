e7bb32410f9d439e8a9c21555a32c1c1213a8e58
Noah Ham <noahham25@gmail.com>
2026-03-30 22:11:32 -0700

    fixed showing index.html

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -1,5 +1,5 @@
 // Scrolls to the contact section when button is clicked
 document.querySelector('#contact').addEventListener('click', function () {
     sessionStorage.setItem('scrollToAbout', '1');
-    window.location.href = '../../index.html';
+    window.location.href = 'https://noahham.com/';
 });
